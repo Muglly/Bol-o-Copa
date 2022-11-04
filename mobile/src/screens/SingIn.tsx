b2b175fc38789d7ca11420afc297caa6a9f659cd
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2022-11-04 11:57:13 -0300

    Criação do button

--- mobile/src/screens/SingIn.tsx
+++ mobile/src/screens/SingIn.tsx
@@ -1,11 +1,19 @@
-import { Center, Text } from "native-base"
+import { Center, Text, Icon } from "native-base"
+import { Fontisto } from "@expo/vector-icons"
 
 import Logo from "../assets/logo.svg"
+import { Button } from "../components/Button"
 
-export function SingIn () {
+export function SingIn() {
     return (
         <Center flex={1} bgColor="gray.900">
-        <Logo width={212} height={40} />
-      </Center>
+            <Logo width={212} height={40} />
+
+            <Button
+                title="Entrar com o google"
+                leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+                type={"SECONDARY"}
+            />
+        </Center>
     )
 }
