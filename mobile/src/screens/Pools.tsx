4834a4d3bf894b7ae889caf3a886b92bbb34fcaf
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2022-11-05 14:59:41 -0300

    Ativado o botão de volta da interface de buscar bolão

--- mobile/src/screens/Pools.tsx
+++ mobile/src/screens/Pools.tsx
@@ -7,17 +7,17 @@ import { Header } from "../components/Header";
 
 
 export function Pools() {
-    const navigation = useNavigation();
+    const { navigate } = useNavigation();
 
     return (
         <VStack flex={1} bgColor="gray.900">
             <Header title="Meus Bolões" />
 
             <VStack mt={6} mx={5} borderBottomWidth={1} borderBottomColor="gray.600" pb={4} mb={4}>
-                <Button 
-                    title="Buscar bolão por código" 
+                <Button
+                    title="Buscar bolão por código"
                     leftIcon={<Icon as={Octicons} name="search" color="black" size="md" />}
-                    onPress={() => navigation.navigate("find")}
+                    onPress={() => navigate("find")}
                 />
             </VStack>
         </VStack>
