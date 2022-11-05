8f494220330ee591a699c02aa10a7951e49b0201
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2022-11-05 10:13:26 -0300

    listagem de bolão dos participantes

--- server/src/routes/pool.ts
+++ server/src/routes/pool.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance } from "fastify"
 import ShortUniqueId from "short-unique-id"
-import { z } from "zod"
+import { tuple, z } from "zod"
 import { prisma } from "../lib/prisma"
 import { authenticate } from "../plugins/authenticate"
 
@@ -103,6 +103,46 @@ export async function poolRoutes(fastify: FastifyInstance) {
         })
 
         return reply.status(201).send()
+    })
+
+    fastify.get("/pools", {
+        onRequest: [authenticate]
+    }, async (request) => {
+        const pools = await prisma.pool.findMany({
+            where: {
+                participants: {
+                    some: {
+                        userId: request.user.sub,
+                    }
+                }
+            },
+            include: {
+                _count: {
+                 select: {
+                    participants: true,
+                 }   
+                },
+                participants: {
+                    select: {
+                        id: true,
+                        user: {
+                            select: {
+                                avatarUrl: true,
+                            }
+                        }
+                    },
+                    take: 4,
+                },
+                owner: {
+                    select: {
+                        id: true,
+                        name: true,
+                    }
+                }
+            }
+        })
+
+        return { pools }
     })
 }
 
